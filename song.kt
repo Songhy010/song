a5ca83f1233dfdb3800370bd94ee43b70ffd7718
Sem Songhy <songhy.s@bizsolution.com.kh>
2021-03-04 23:33:17 +0700

    first commit

---
+++ song.kt
@@ -0,0 +1,25 @@
+/*
+* This file is kotin file I create to for complete PIRPLE Assignment
+* This file use to create and print variable my favorite song attribute
+*/
+fun main(){
+    // this variable store value for song name attribute
+    val songName : String = "Your Man"
+    // this variable store value for artist attribute
+    val artist : String = "Josh Turner"
+    // this variable store value for artist gender attribute
+    val artistSex : Char = 'M'
+    // this variable store value for year released attribute
+    val released : Int = 2009
+    // this variable store value for genre attribute
+    val genre : String = "Pop"
+    // this variable store value for duration attribute
+    val duration : Float = 3.20f
+
+    print("Song Name: ${songName}")
+    print("Artist: ${artist}")
+    print("Artist Sex: ${artistSex}")
+    print("Released: ${released}")
+    print("Genre: ${genre}")
+    print("Duration: ${duration}")
+}
